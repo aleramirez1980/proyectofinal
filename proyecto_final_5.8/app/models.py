f4cf5f2984166cc662ca88bcfa959d3ec615013c
Nicolás Puebla <njpuebla@gmail.com>
2024-08-10 17:46:09 -0300

    Agregué VBC de eléctricas y modifiqué todos los modelos de la app

--- proyecto_final_5.8/app/models.py
+++ proyecto_final_5.8/app/models.py
@@ -4,15 +4,32 @@ from django.db import models
 class Electrica(models.Model):
     marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
+    serial = models.IntegerField(null=True)
+    anio = models.IntegerField(null=True)
+    precio = models.IntegerField(null=True)
+    descripcion = models.CharField(max_length=200, null=True)
 
 class Acustica(models.Model):
     marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
+    serial = models.IntegerField(null=True)
+    anio = models.IntegerField(null=True)
+    precio = models.IntegerField(null=True)
+    descripcion = models.CharField(max_length=200, null=True)
 
 class Amplificador(models.Model):
     marca = models.CharField(max_length=50)
-    modelo = models.CharField(max_length=50) 
+    modelo = models.CharField(max_length=50)
+    potencia = models.IntegerField(null=True)
+    serial = models.IntegerField(null=True)
+    anio = models.IntegerField(null=True)
+    precio = models.IntegerField(null=True)
+    descripcion = models.CharField(max_length=200, null=True) 
     
 class Efecto(models.Model):
     marca = models.CharField(max_length=50)
-    modelo = models.CharField(max_length=50)   
+    modelo = models.CharField(max_length=50)
+    serial = models.IntegerField(null=True)
+    anio = models.IntegerField(null=True)
+    precio = models.IntegerField(null=True)
+    descripcion = models.CharField(max_length=200, null=True)   
