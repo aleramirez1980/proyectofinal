e4c8226fcdd18044c7bfde376e25e7340e6fb45a
Nicolás Puebla <njpuebla@gmail.com>
2024-08-11 00:57:07 -0300

    Agregué las VBC de acústicas, amplificadores y efectos

--- proyecto_final_5.8/vbc/urls.py
+++ proyecto_final_5.8/vbc/urls.py
@@ -6,5 +6,23 @@ urlpatterns = [
     path("electricas/nuevo", views.ElectricaCreateView.as_view(), name = "NuevaElectrica"),
     path("electricas/<pk>", views.ElectricaDetailView.as_view(), name = "DetalleElectrica"),
     path("electricas/<pk>/editar", views.ElectricaUpdateView.as_view(), name = "EditarElectrica"),
-    path("electricas/<pk>/borrar", views.ElectricaDeleteView.as_view(), name = "BorrarElectrica"),   
+    path("electricas/<pk>/borrar", views.ElectricaDeleteView.as_view(), name = "BorrarElectrica"),
+    
+    path("acusticas/lista", views.AcusticaListView.as_view(), name = "ListaAcusticas"),
+    path("acusticas/nuevo", views.AcusticaCreateView.as_view(), name = "NuevaAcustica"),
+    path("acusticas/<pk>", views.AcusticaDetailView.as_view(), name = "DetalleAcustica"),
+    path("acusticas/<pk>/editar", views.AcusticaUpdateView.as_view(), name = "EditarAcustica"),
+    path("acusticas/<pk>/borrar", views.AcusticaDeleteView.as_view(), name = "BorrarAcustica"),
+
+    path("amplificadores/lista", views.AmplificadorListView.as_view(), name = "ListaAmplificadores"),
+    path("amplificadores/nuevo", views.AmplificadorCreateView.as_view(), name = "NuevoAmplificador"),
+    path("amplificadores/<pk>", views.AmplificadorDetailView.as_view(), name = "DetalleAmplificador"),
+    path("amplificadores/<pk>/editar", views.AmplificadorUpdateView.as_view(), name = "EditarAmplificador"),
+    path("amplificadores/<pk>/borrar", views.AmplificadorDeleteView.as_view(), name = "BorrarAmplificador"),  
+
+    path("efectos/lista", views.EfectoListView.as_view(), name = "ListaEfectos"),
+    path("efectos/nuevo", views.EfectoCreateView.as_view(), name = "NuevoEfecto"),
+    path("efectos/<pk>", views.EfectoDetailView.as_view(), name = "DetalleEfecto"),
+    path("efectos/<pk>/editar", views.EfectoUpdateView.as_view(), name = "EditarEfecto"),
+    path("efectos/<pk>/borrar", views.EfectoDeleteView.as_view(), name = "BorrarEfecto"),   
 ]
--- proyecto_final_5.8/vbc/views.py
+++ proyecto_final_5.8/vbc/views.py
@@ -5,6 +5,9 @@ from django.urls import reverse_lazy
 
 
 from app.models import Electrica
+from app.models import Acustica
+from app.models import Amplificador
+from app.models import Efecto
 
 class ElectricaListView(ListView):
     model = Electrica
@@ -31,3 +34,81 @@ class ElectricaDeleteView(DeleteView):
     model = Electrica
     template_name = "vbc/electricas_borrar.html"
     success_url = reverse_lazy("ListaElectricas")
+
+class AcusticaListView(ListView):
+    model = Acustica
+    context_object_name = "Acusticas"
+    template_name = "vbc/lista_acusticas.html"
+    
+class AcusticaDetailView(DetailView):
+    model = Acustica
+    template_name = "vbc/acusticas_detalle.html"
+    
+class AcusticaCreateView(CreateView):
+    model = Acustica
+    template_name = "vbc/acusticas_crear.html"
+    success_url = reverse_lazy("ListaAcusticas")
+    fields = ["marca", "modelo", "serial", "anio", "precio", "descripcion"]
+    
+class AcusticaUpdateView(UpdateView):
+    model = Acustica
+    template_name = "vbc/acusticas_editar.html"
+    success_url = reverse_lazy("ListaAcusticas")
+    fields = ["marca", "modelo", "serial", "anio", "precio", "descripcion"] 
+    
+class AcusticaDeleteView(DeleteView):
+    model = Acustica
+    template_name = "vbc/acusticas_borrar.html"
+    success_url = reverse_lazy("ListaAcusticas")
+
+class AmplificadorListView(ListView):
+    model = Amplificador
+    context_object_name = "Amplificadores"
+    template_name = "vbc/lista_amplificadores.html"
+    
+class AmplificadorDetailView(DetailView):
+    model = Amplificador
+    template_name = "vbc/amplificadores_detalle.html"
+    
+class AmplificadorCreateView(CreateView):
+    model = Amplificador
+    template_name = "vbc/amplificadores_crear.html"
+    success_url = reverse_lazy("ListaAmplificadores")
+    fields = ["marca", "modelo", "potencia", "serial", "anio", "precio", "descripcion"]
+    
+class AmplificadorUpdateView(UpdateView):
+    model = Amplificador
+    template_name = "vbc/amplificadores_editar.html"
+    success_url = reverse_lazy("ListaAmplificadores")
+    fields = ["marca", "modelo", "potencia", "serial", "anio", "precio", "descripcion"] 
+    
+class AmplificadorDeleteView(DeleteView):
+    model = Amplificador
+    template_name = "vbc/amplificadores_borrar.html"
+    success_url = reverse_lazy("ListaAmplificadores")
+
+class EfectoListView(ListView):
+    model = Efecto
+    context_object_name = "Efectos"
+    template_name = "vbc/lista_efectos.html"
+    
+class EfectoDetailView(DetailView):
+    model = Efecto
+    template_name = "vbc/efectos_detalle.html"
+    
+class EfectoCreateView(CreateView):
+    model = Efecto
+    template_name = "vbc/efectos_crear.html"
+    success_url = reverse_lazy("ListaEfectos")
+    fields = ["marca", "modelo", "serial", "anio", "precio", "descripcion"]
+    
+class EfectoUpdateView(UpdateView):
+    model = Efecto
+    template_name = "vbc/efectos_editar.html"
+    success_url = reverse_lazy("ListaEfectos")
+    fields = ["marca", "modelo", "serial", "anio", "precio", "descripcion"] 
+    
+class EfectoDeleteView(DeleteView):
+    model = Efecto
+    template_name = "vbc/efectos_borrar.html"
+    success_url = reverse_lazy("ListaEfectos")
